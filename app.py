d2bd1a11621d7bfbee1ee2cd3261ffc4bced5553
leif <07lfindlay@brightoncollege.net>
2021-10-18 19:38:26 +0100

    Everything works rn. Gonna try and make links function properly now

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from neo4j import GraphDatabase, basic_auth
+app = Flask(__name__)
+
+
+def makeQuery(q):
+    driver = GraphDatabase.driver(
+      "bolt://3.235.105.50",
+      auth=basic_auth("neo4j", "twirls-cargoes-misleads"))
+    cypher_query = q
+    with driver.session(database="neo4j") as session:
+      results = session.read_transaction(
+        lambda tx: tx.run(cypher_query).data())
+      #for record in results:
+        #print(record['n'])
+    driver.close()
+    return results
+
+
+
+
+@app.route('/')
+def main():
+    """yoooooooo"""
+    results = makeQuery('''
+    MATCH (n:Text)
+    RETURN n
+    ''')
+    books = [(record['n']) for record in results]
+    y = type(books[0])
+    #print(books[0]['title'])
+    return render_template('main.html', books=books)
+
+
+@app.route('/text/<string:text_name>')
+def text(text_name):
+    q = '''
+    MATCH (n:Text {title: '%s'})
+    RETURN n
+    ''' % (str(text_name))
+    text_record = makeQuery(q)
+    print(text_record)
+    return render_template('text.html', text_name=text_name, text_record=text_record)
+
+
+@app.route('/graph')
+def graph():
+    print("hi")
+    return render_template('graph.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
---
+++ templates/grapher.js
@@ -0,0 +1,119 @@
+function draw() {
+var image3 = "https://upload.wikimedia.org/wikipedia/commons/a/ab/JoyceUlysses2.jpg";
+var container = document.getElementById("mynetwork");
+//var container = document.querySelector("#graph");
+
+  var nodes = new vis.DataSet([
+    {
+      id: 1,
+      shape: "circularImage",
+      image:
+          "https://i.guim.co.uk/img/media/fa5c01599c7fa657c2c5b8aa501143c55b5cb89e/0_347_2346_1407/master/2346.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=96cb6381e98c6c249e108162b784bbb2",
+      label: "James Joyce",
+      fixed: true
+    },
+    {
+      id: 2,
+      shape: "circularImage",
+      image: "https://lenguajecss.com/img/css3-logo.png",
+      label: "CSS3",
+      fixed: true
+    },
+    {
+      id: 3,
+      shape: "circularImage",
+      image: "image",
+      label: "A Portrait of the Artist as a Young Man",
+      fixed: true
+    },
+    {
+      id: 4,
+      shape: "circularImage",
+      image: "http://www.freeiconspng.com/uploads/less-icon-17.png",
+      label: "LESS",
+      fixed: true
+    },
+    {
+      id: 5,
+      shape: "circularImage",
+      image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/4a/Joyce_-_Dubliners%2C_1914_-_3690390_F.jpg",
+      label: "Dubliners",
+      fixed: true
+    },
+    {
+      id: 6,
+      shape: "circularImage",
+      image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/This_Side_of_Paradise_Cover_1920_Retouched.jpg/1200px-This_Side_of_Paradise_Cover_1920_Retouched.jpg",
+      label: "This Side of Paradise",
+      fixed: true
+    },
+    {
+      id: 7,
+      shape: "circularImage",
+      image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/PostCSS_Logo.svg/2000px-PostCSS_Logo.svg.png",
+      label: "Odyssey",
+      fixed: true
+    },
+    {
+      id: 8,
+      shape: "circularImage",
+      image: image3,
+      label: "Ulysses",
+      fixed: true,
+      url: "https://en.wikipedia.org/wiki/Ulysses_(novel)"
+    }
+  ]);
+
+  var edges = new vis.DataSet([
+    {id: 1, from: 1, to: 8, label: "WROTE"},
+    {from: 1, to: 3},
+    {from: 2, to: 4},
+    {from: 1, to: 5},
+    {from: 3, to: 6},
+    {from: 2, to: 7},
+    {from: 2, to: 8}
+  ]);
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+
+  var options = {
+    nodes: {
+      borderWidth: 0,
+      size: 42,
+      color: {
+        border: "#222",
+        background: "transparent"
+      },
+      font: {
+        color: "#111",
+        face: "Helvetic",
+        size: 16,
+        strokeWidth: 1,
+        strokeColor: "#222"
+      }
+    },
+    edges: {
+      color: {
+        color: "#CCC",
+        highlight: "#A22"
+      },
+      width: 5,
+      length: 275,
+      hoverWidth: 0.05
+    }
+  };
+
+  var network = new vis.Network(container, data, options);
+  network.on("selectNode", function (params) {
+    if (params.nodes.length === 1) {
+      var node = nodes.get(params.nodes[0]);
+      window.open(node.url, "_blank");
+    }
+    network.unselectAll();
+  });
+}
