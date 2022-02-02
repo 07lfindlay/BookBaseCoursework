2e0c380c878873c991ee6fda605874e73f37b4be
07lfindlay <47146518+07lfindlay@users.noreply.github.com>
2022-02-02 10:37:55 +0000

    Update Search.js

--- static/Search.js
+++ static/Search.js
@@ -1,16 +1,16 @@
 
 function Search() {
-    var searchtext = document.getElementById('SearchBar').value;
+    var searchtext = document.getElementById('SearchBar').value; //Get value from SearchBar text field
     //console.log(searchtext);
     let nodes;
-    fetch(`/Searcher?searchterms=${searchtext}`, {method: 'POST'})
+    fetch(`/Searcher?searchterms=${searchtext}`, {method: 'POST'}) //Send a request to the Flask function under '/Searcher', passing the search value as arg.
         .then(data => data.json()).then(json => {console.log(json);
-            nodes = json;
+            nodes = json; // set nodes value to the value returned by the '/Searcher' function
             console.log(nodes);
             console.log(typeof nodes)
-            let newnodes = compile(nodes, [], "stuff", true);
+            let newnodes = compile(nodes, [], "stuff", true); //Convert the nodes into a format that can be drawn
             console.log(newnodes)
-            draw(newnodes, []);
+            draw(newnodes, []); //Update the graph, drawing only the searched nodes
 
         })
 }
